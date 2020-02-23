13e0a7851fee40a5867cdddbaf00f9177fd8dcaa
George Stambulyants <george.stamb28@gmail.com>
2020-02-23 17:41:49 +0300

    Added weather data

--- FlaskByExamplesBook/headlines/headlines.py
+++ FlaskByExamplesBook/headlines/headlines.py
@@ -2,8 +2,7 @@ from flask import Flask, request
 import feedparser
 from flask import render_template
 import json
-import urllib2
-import urllib
+import urllib.request as urllib
 
 
 app = Flask(__name__)
@@ -13,24 +12,47 @@ RSS_FEEDS = {'bbc': 'https://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://rss.iol.io/iol/news'}
 
-
-def get_weather(query):
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={},\
-                uk&units=metric&appid=a27886fec137fb6e69e4a5697fd918df'
-    query = urllib.quote(query)
-    url = api_url.format(query)
+DEFAULTS = {'publication': 'bbc', 'city': 'London, UK'}
 
 
 @app.route('/')
-def get_news():
-    query = request.args.get('publication')
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
+    return render_template('home.html', articles=articles, weather=weather)
+
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}uk&units=metric&appid=a27886fec137fb6e69e4a5697fd918df'
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description': parsed['weather'][0]['description'],
+                'temperature': parsed['main']['temp'],
+                'city': parsed['name'], 'country': parsed['sys']['country']}
+    return weather
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'bbc'
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    # first_article = feed['entries'][0]
-    return render_template('home.html', articles=feed['entries'])
+    return feed['entries']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
