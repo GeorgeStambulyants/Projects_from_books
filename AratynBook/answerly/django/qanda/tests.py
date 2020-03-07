49165a95dbef813a03ec4eca4a32736be59fd0ba
George Stambulyants <george.stamb28@gmail.com>
2020-03-07 21:06:56 +0300

    Testing DailyQuestionList view. assertInHTML failed. Can't figure why

--- AratynBook/answerly/django/qanda/tests.py
+++ AratynBook/answerly/django/qanda/tests.py
@@ -1,11 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from unittest.mock import patch
-
+from datetime import date
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from elasticsearch import Elasticsearch
 from qanda.models import Question
+from qanda.views import DailyQuestionList
+from qanda.factories import QuestionFactory
 
+
+QUESTION_CREATED_STRFTIME = '%Y-%m-%d %H:%M'
 
 class QuestionSaveTestCase(TestCase):
     '''
@@ -46,3 +50,40 @@ class QuestionSaveTestCase(TestCase):
                 'doc_as_upsert': True,
             }
         )
+
+
+class DailyQuestionTestCase(TestCase):
+    '''
+    Tests the DailyQuestionList view
+    '''
+    QUESTION_LIST_NEEDLE_TEMPLATE = '''
+    <li>
+        <a href="/q/{id}">{title}</a>
+        by {username} on {date}
+    </li>
+    '''
+
+    REQUEST = RequestFactory().get(path='/q/2030-12-31')
+    TODAY = date.today()
+
+    def test_GET_on_day_with_many_questions(self):
+        todays_questions = [QuestionFactory() for _ in range(10)]
+
+        response = DailyQuestionList.as_view()(
+            self.REQUEST,
+            year=self.TODAY.year,
+            month=self.TODAY.month,
+            day=self.TODAY.day,
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(10, response.context_data['object_list'].count())
+        rendered_content = response.rendered_content
+        for question in todays_questions:
+            needle = self.QUESTION_LIST_NEEDLE_TEMPLATE.format(
+                id=question.id,
+                title=question.title,
+                username=question.user.username,
+                date=question.created.strftime(QUESTION_CREATED_STRFTIME)
+            )
+            self.assertInHTML(needle, rendered_content)
