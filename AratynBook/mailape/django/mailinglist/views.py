2fd3b4d01177afba3d5490d1c29d34d27cf9dfb0
George Stambulyants <george.stamb28@gmail.com>
2020-03-08 18:23:09 +0300

    Created MailingList views and templates:

--- AratynBook/mailape/django/mailinglist/views.py
+++ AratynBook/mailape/django/mailinglist/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+)
+from django.views.generic import (
+    ListView, CreateView, DeleteView, DetailView,
+)
+from mailinglist.models import (
+    MailingList,
+)
+from mailinglist.forms import (
+    MailingListForm,
+)
+from mailinglist.mixins import (
+    UserCanUseMailingList,
+)
+from django.urls import (
+    reverse_lazy,
+)
 
-# Create your views here.
+
+class MailingListListView(LoginRequiredMixin, ListView):
+
+    def get_queryset(self):
+        return MailingList.objects.filter(owner=self.request.user)
+    
+
+class CreateMailingListView(LoginRequiredMixin, CreateView):
+    form_class = MailingListForm
+    template_name = 'mailinglist/mailinglist_form.html'
+
+    def get_initial(self):
+        return {
+            'owner': self.request.user.id,
+        }
+
+
+class DeleteMailingListView(LoginRequiredMixin, UserCanUseMailingList,
+                            DeleteView):
+    model = MailingList
+    success_url = reverse_lazy('mailinglist:mailinglist_list')
+
+
+class MailingListDetailView(LoginRequiredMixin, UserCanUseMailingList,
+                            DetailView):
+    model = MailingList
