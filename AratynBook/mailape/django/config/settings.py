dff4ce955dfa7b8c20f086807925fed4b3bcd4e6
George Stambulyants <george.stamb28@gmail.com>
2020-03-08 15:20:20 +0300

    A base template was added and configured. <<AratynBook/mailape/>>

--- AratynBook/mailape/django/config/settings.py
+++ AratynBook/mailape/django/config/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configuring crispy_forms to use bootstrap4
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
