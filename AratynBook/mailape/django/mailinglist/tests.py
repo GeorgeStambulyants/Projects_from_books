719e142973c2bb689e8ce0aee0517af536047e4b
George Stambulyants <george.stamb28@gmail.com>
2020-03-09 14:47:04 +0300

    Testing code that uses Celery tasks. All tests failed. Don't know how celery workd((. TODO: Need to read the docs. <<AratynBook/mailape>>

--- AratynBook/mailape/django/mailinglist/tests.py
+++ AratynBook/mailape/django/mailinglist/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from unittest.mock import patch
+from mailinglist.models import Subscriber, MailingList
+from django.contrib.auth import get_user_model
+from mailinglist.factories import SubscriberFactory
 
-# Create your tests here.
+
+class MockSendEmailToSubscriberTask:
+
+    def setUp(self):
+        self.send_confirmation_email_patch = patch(
+            'mailinglist.tasks.send_confirmation_email_to_subscriber'
+        )
+        self.send_confirmation_email_mock = self.send_confirmation_email_patch.start()
+        super().setUp()
+
+    def tearDown(self):
+        self.send_confirmation_email_patch.stop()
+        self.send_confirmation_email_mock = None
+        super().tearDown()
+
+
+class SubscriberCreationTestCase(MockSendEmailToSubscriberTask, TestCase):
+
+    def test_calling_create_queues_confirmation_email_task(self):
+        user = get_user_model().objects.create_user(
+            username='unit test runner'
+        )
+        mailing_list = MailingList.objects.create(
+            name='unit test',
+            owner=user,
+        )
+        Subscriber.objects.create(
+            email='unittest@example.com',
+            mailing_list=mailing_list
+        )
+
+        self.assertEqual(self.send_confirmation_email_mock.delay.call_count, 1)
+
+
+class SubscriberManagerTestCase(TestCase):
+    
+    def testConfirmedSubscribedForMailingList(self):
+        mailing_list = MailingList.objects.create(
+            name='unit test',
+            owner=get_user_model().objects.create_user(
+                username='unit test'
+            )
+        )
+        confirmed_users = [
+            SubscriberFactory(mailing_list=mailing_list)
+            for _ in range(3)
+        ]
+        confirmed_users_qs = Subscriber.objects.confirmed_subscribers_for_mailing_list(
+            mailing_list=mailing_list
+        )
+        self.assertEqual(
+            len(confirmed_users),
+            confirmed_users_qs.count()
+        )
+        for user in confirmed_users_qs:
+            self.assertIn(user, confirmed_users)
