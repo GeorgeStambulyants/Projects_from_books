1692b5c447a30b84d78175c1aafa229a2d2b0960
George Stambulyants <george.stamb28@gmail.com>
2020-03-09 14:20:29 +0300

    Sending messages to subscribers. Need some debugging. <<AratynBook/mailape/>>

--- AratynBook/mailape/django/mailinglist/emails.py
+++ AratynBook/mailape/django/mailinglist/emails.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.template import Context, engines
 from django.shortcuts import reverse
 from django.core.mail import send_mail
+from datetime import datetime
+from django.utils.datetime_safe import datetime
 
 
 CONFIRM_SUBSCRIPTION_HTML = 'mailinglist/email/confirmation.html'
 CONFIRM_SUBSCRIPTION_TXT = 'mailinglist/email/confirmation.txt'
+SUBSCRIBER_MESSAGE_HTML = 'mailing/email/subscriber_message.html'
+SUBSCRIBER_MESSAGE_TXT = 'mailing/email/subscriber_message.txt'
 
 
 class EmailTemplateContext(Context):
@@ -20,7 +24,7 @@ class EmailTemplateContext(Context):
         email_ctx = self.common_context(subscriber)
         email_ctx.update(dict_)
         super().__init__(email_ctx, **kwargs)
-    
+    SUBSCRIBER_MESSAGE_TXT = 'mailing/email/subscriber_message.txt'
     def common_context(self, subscriber):
         subscriber_pk_kwargs = {'pk': subscriber.id}
         unsubscribe_path = reverse(
@@ -61,3 +65,33 @@ def send_confirmation_email(subscriber):
         recipient_list=(subscriber.email,),
         html_message=html_body,
     )
+
+
+def send_subscriber_message(subscriber_message):
+    message = subscriber_message.message
+    context = EmailTemplateContext(subscriber_message.subscriber, {
+        'body': message.body,
+    })
+
+    dt_engine = engines['django'].engine
+    text_body_template = dt_engine.get_template(SUBSCRIBER_MESSAGE_TXT)
+    text_body = text_body_template.render(context=context)
+    html_body_tempalte = dt_engine.get_template(SUBSCRIBER_MESSAGE_HTML)
+    html_body = html_body_tempalte.render(context=context)
+
+    utcnow = datetime.utcnow()
+
+    subscriber_message.last_attempt = utcnow
+    subscriber_message.save()
+
+    success = send_mail(
+        subject=message.subject,
+        message=text_body,
+        from_email=settings.MAILING_LIST_FROM_EMAIL,
+        recipient_list=(subscriber_message.subscriber.email,),
+        html_message=html_body,
+    )
+
+    if success == 1:
+        subscriber_message.sent = utcnow
+        subscriber_message.save()
