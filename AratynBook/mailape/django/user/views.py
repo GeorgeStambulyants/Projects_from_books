c6038811e0aae89cc3094ce054a8fd610e783c28
George Stambulyants <george.stamb28@gmail.com>
2020-03-09 11:29:53 +0300

    Added get_success_url to RegisterView to redirect after registration. <<AratynBook/mailape/>>

--- AratynBook/mailape/django/user/views.py
+++ AratynBook/mailape/django/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
@@ -6,3 +6,6 @@ from django.views.generic.edit import CreateView
 class RegisterView(CreateView):
     template_name = 'user/register.html'
     form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse('user:login')
