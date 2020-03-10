eca472c2d19300179dbef6e51121ed1127f4d627
George Stambulyants <george.stamb28@gmail.com>
2020-03-10 15:02:40 +0300

    API permissions bug fixing. <<AratynBook/mailape>>

--- AratynBook/mailape/django/mailinglist/permissions.py
+++ AratynBook/mailape/django/mailinglist/permissions.py
@@ -7,7 +7,7 @@ class CanUseMailingList(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        if isinstance(pbj, Subscriber):
+        if isinstance(obj, Subscriber):
             return obj.mailing_list.user_can_use_mailing_list(user)
         elif isinstance(obj, MailingList):
             return obj.user_can_use_mailing_list(user)
