f8812e219bfb2c2a93de8d698f09bee7d7c2a165
George Stambulyants <george.stamb28@gmail.com>
2020-03-16 19:40:41 +0300

    Functional test now checks the possibility to enter list's element. <<TDD_with_Python>>

--- PercivalBook_-TDD-_/chapter_1_2/superlists/functional_tests.py
+++ PercivalBook_-TDD-_/chapter_1_2/superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -18,17 +20,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # She sees that the title and the header of the page reference the todo-list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is suggested to enter new element of the list
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She prints in the text fields 'Buy fruits'
+        inputbox.send_keys('Buy fruits')
 
         # When she hits enter button page refreshes and now it has:
         # '1: Buy fruits' in the list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy fruits' for row in rows)
+        )
 
         # The text fields is still on page ready to another input
         # Edit enters 'Buy vegitables'
+        self.fail('Finish the test!')
 
         # Page refreshesassert 'Django' in browser.title again and now there are two elements in the list
 
