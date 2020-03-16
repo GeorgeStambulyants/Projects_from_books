1677765dc7769dc6959ffb2d0b13e2b8d7ff6cb1
George Stambulyants <george.stamb28@gmail.com>
2020-03-16 16:05:04 +0300

    Newspaper app was added. <<VincentBook/news>>

--- VincentBook/news/newspaper_project/newspaper_project/settings.py
+++ VincentBook/news/newspaper_project/newspaper_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 
     # 3rd Party
     'crispy_forms',
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
