22bf16f50211b002119b3e761c90ba258873246d
George Stambulyants <george.stamb28@gmail.com>
2020-03-18 15:49:16 +0300

    Added post_list and post_detail view functions to blog app. Also added routing. <<MeleBook/Blog>>

--- MeleBook/Blog/mysite/blog/models.py
+++ MeleBook/Blog/mysite/blog/models.py
@@ -39,3 +39,15 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ]
+        )
+    
+    
