62f624ab3d77eddaa2e388f54bd7ca39d367984b
George Stambulyants <george.stamb28@gmail.com>
2020-03-18 17:33:55 +0300

    Created EmailPostForm and post_share view func. <<MeleBook/Blog>>

--- MeleBook/Blog/mysite/blog/views.py
+++ MeleBook/Blog/mysite/blog/views.py
@@ -7,6 +7,9 @@ from django.core.paginator import (
 from django.views.generic import (
     ListView,
 )
+from .forms import (
+    EmailPostForm,
+)
 from .models import (
     Post,
 )
@@ -47,3 +50,19 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 1
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid()
+        cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    
+    return render(
+        request,
+        'blog/post/share.html',
+        {'post': post, 'form': form}
+    )
