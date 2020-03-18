8c35a35054ad28d6457df4beed9417489bb2ab17
George Stambulyants <george.stamb28@gmail.com>
2020-03-18 17:42:53 +0300

    Sending email. <<MeleBook/Blog>>

--- MeleBook/Blog/mysite/blog/views.py
+++ MeleBook/Blog/mysite/blog/views.py
@@ -7,6 +7,9 @@ from django.core.paginator import (
 from django.views.generic import (
     ListView,
 )
+from django.core.mail import (
+    send_mail,
+)
 from .forms import (
     EmailPostForm,
 )
@@ -54,15 +57,23 @@ class PostListView(ListView):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
-        if form.is_valid()
-        cd = form.cleaned_data
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.\
+                format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.\
+                format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@my_blog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     
     return render(
         request,
         'blog/post/share.html',
-        {'post': post, 'form': form}
+        {'post': post, 'form': form, 'sent': sent}
     )
