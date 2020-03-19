0801ecaf84dd9f019f130de3c2159b2e3ee82920
George Stambulyants <george.stamb28@gmail.com>
2020-03-19 18:38:52 +0300

    Added new test to functional tests, which checks that multiple users can start lists at different urls. <<TDD_with_Python>>

--- PercivalBook_-TDD-_/chapter_1_2/superlists/functional_tests/tests.py
+++ PercivalBook_-TDD-_/chapter_1_2/superlists/functional_tests/tests.py
@@ -31,7 +31,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edit heard about new cool applications with todo-lists
+        # Edith heard about new cool applications with todo-lists
 
         # She decides check out the home page ot the application
         self.browser.get(self.live_server_url)
@@ -58,7 +58,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy fruits')
 
         # The text fields is still on page ready to another input
-        # Edit enters 'Buy vegitables'
+        # Edith enters 'Buy vegitables'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy vegetables')
         inputbox.send_keys(Keys.ENTER)
@@ -66,13 +66,54 @@ class NewVisitorTest(LiveServerTestCase):
         # Page refreshes again and now there are two elements in the list
         self.wait_for_row_in_list_table('1: Buy fruits')
         self.wait_for_row_in_list_table('2: Buy vegetables')
-        # Edit is interested if the site will remember her todo-list
+        # Edith is interested if the site will remember her todo-list
         # Hey, now she sees that our site generated for her unique URL - there
         # is a little message on the page with this information
-        self.fail('Finish the test!')
+        ## self.fail('Finish the test!')
         # She goes to that URL and her list is there
 
-        # Edit is satisfied now and she goes to bed
+        # Edith is satisfied now and she goes to bed
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts with a new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy fruits')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy fruits')
+
+        # She notices that a list has unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now new user, Francis, visits the site
+        
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        # Francis visits home page. There is no Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('But fruits', page_text)
+        self.assertNotIn('But vegetables', page_text)
+
+        # Francis starts new list
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # And again, there is no Edith's list here
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy fruits', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+        # Now they both go to sleep
 
 
 if __name__ == "__main__":
