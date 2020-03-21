36174654f2ff1bf98ffa10dcadd1e7b80a295353
George Stambulyants <george.stamb28@gmail.com>
2020-03-21 12:09:54 +0300

    Added messages to edit page. <<MeleBook/Bookmarks>>

--- MeleBook/Bookmarks/bookmarks/account/views.py
+++ MeleBook/Bookmarks/bookmarks/account/views.py
@@ -10,7 +10,9 @@ from django.contrib.auth import (
 from django.contrib.auth.decorators import (
     login_required,
 )
-
+from django.contrib import (
+    messages,
+)
 from .forms import (
     LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 )
@@ -91,6 +93,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)\
