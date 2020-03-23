490ae292e04443ca15a84c4bbd0e0416e5139306
George Stambulyants <george.stamb28@gmail.com>
2020-03-23 19:06:40 +0300

    Added thumbnails. Used Django channels. <<MaraniBook>>

--- MaraniBook/booktime/booktime/main/models.py
+++ MaraniBook/booktime/booktime/main/models.py
@@ -16,7 +16,9 @@ class ProductImage(models.Model):
         Product, on_delete=models.CASCADE,
     )
     image = models.ImageField(upload_to='product-images')
-
+    thumbnail = models.ImageField(
+        upload_to='products-thumbnails', null=True,
+    )
 
 class ProductTag(models.Model):
     products = models.ManyToManyField(Product, blank=True)
