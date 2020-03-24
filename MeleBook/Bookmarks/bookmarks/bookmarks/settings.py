79913d352d9a0ff152d235b4acadd8831fbe2cf2
George Stambulyants <george.stamb28@gmail.com>
2020-03-24 12:24:11 +0300

    Added new app actions. <<MeleBook/Bookmarks>>

--- MeleBook/Bookmarks/bookmarks/bookmarks/settings.py
+++ MeleBook/Bookmarks/bookmarks/bookmarks/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
 
     'social_django',
     'sorl.thumbnail',
