2a520ab065f68754b3b24bc020b736574c86cb38
George Stambulyants <george.stamb28@gmail.com>
2020-03-25 14:32:40 +0300

    Added views and templates to handle cart. <<MeleBook?MyShop>>

--- MeleBook/MyShop/myshop/shop/views.py
+++ MeleBook/MyShop/myshop/shop/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import (
 from .models import (
     Category, Product,
 )
+from cart.forms import (
+    CartAddProductForm,
+)
 
 
 def product_list(request, category_slug=None):
@@ -26,10 +29,12 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
         context={
             'product': product,
+            'cart_product_form': cart_product_form,
         }
     )
