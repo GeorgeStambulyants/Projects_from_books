c53b3c426a8fe0759911ab2d80aeae5e4ea93537
George Stambulyants <george.stamb28@gmail.com>
2020-03-25 13:32:35 +0300

    Some debug deletings. <<MeleBook/MyShop>>

--- MeleBook/MyShop/myshop/shop/tests.py
+++ MeleBook/MyShop/myshop/shop/tests.py
@@ -1,8 +1,1 @@
 from django.test import TestCase
-
-
-class Test(TestCase):
-
-    def test_right_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'shop/product/list.html')
