978bd9ece2b9f8f824082ee99f47ce99359b3f36
George Stambulyants <george.stamb28@gmail.com>
2020-03-25 13:57:32 +0300

    Added new app to let users use the cart. <<MeleBook/MyShop>>

--- MeleBook/MyShop/myshop/myshop/settings.py
+++ MeleBook/MyShop/myshop/myshop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
