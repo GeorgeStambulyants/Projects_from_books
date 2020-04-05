e8f46245b1ae7a988f2b4fae1a1bbbc7a3d574dc
George Stambulyants <george.stamb28@gmail.com>
2020-04-05 13:18:21 +0300

    Used Bootstrap to improve layout. <<TDD with Python>>

--- TDD_with_Python/part_1/superlists/functional_tests/tests.py
+++ TDD_with_Python/part_1/superlists/functional_tests/tests.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 import time
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -113,8 +113,6 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-        self.fail('Finish the test!')
 
         # Satisfied, they both go back to sleep
     
