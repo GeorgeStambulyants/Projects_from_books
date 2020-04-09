e130ca7a13a98b3cbb4a716b30a44257491fb78c
George Stambulyants <george.stamb28@gmail.com>
2020-04-09 14:23:36 +0300

    Refactored list view to handle new item POSTs. <<TDD with Python>>

--- TDD_with_Python/part_1/superlists/lists/views.py
+++ TDD_with_Python/part_1/superlists/lists/views.py
@@ -11,7 +11,9 @@ def home_page(request):
 
 def view_list(request, id):
     list_ = List.objects.get(id=id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     
     return render(
         request,
@@ -32,10 +34,4 @@ def new_list(request):
         list_.delete()
         error = 'You can\'t have an empty list item'
         return render(request, 'home.html', {'error': error})
-    return redirect(f'/lists/{list_.id}/')
-
-
-def add_item(request, id):
-    list_ = List.objects.get(id=id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
