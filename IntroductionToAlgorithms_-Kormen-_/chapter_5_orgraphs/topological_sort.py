0fea71f66c9afcf0095f84ecd5039c9e1d152b99
George Stambulyants <george.stamb28@gmail.com>
2020-04-13 17:45:15 +0300

    Topological sort improved. <<Introduction to the algorithms>>

--- IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/topological_sort.py
+++ IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/topological_sort.py
@@ -1,53 +1,55 @@
 # Topological sort
 #
-# Time: O(n + m), where n is the number of vertices 
+# Time: O(n + m), where n is the number of vertices
 # and m is the number if edges
 
 
 class Graph:
     '''
         oriented acyclic graph
-        degree of vertex - the number of edges entering the vertex
+        in-degree of vertex - the number of edges entering the vertex
     '''
 
     def __init__(self, number_of_vertices):
+        '''
+            Indexes in the list self.vertex_in_degrees represent
+            respective vertices
+            and each element is in_degree
+        '''
         self.number_of_vertices = number_of_vertices
         self.graph = {
-            vertex: [] for vertex in range(1, self.number_of_vertices + 1)
+            vertex: [] for vertex in range(self.number_of_vertices)
         }
-        self.vertex_degrees = {
-            vertex: 0 for vertex in range(1, self.number_of_vertices + 1)
-        }
+        self.vertex_in_degree = [0] * self.number_of_vertices
 
     def add_edge(self, from_, to):
         self.graph[from_].append(to)
-        self.vertex_degrees[to] += 1
+        self.vertex_in_degree[to] += 1
 
     def get_vertex_degree(self, vertex):
-        return self.vertex_degrees[vertex]
+        return self.vertex_in_degree[vertex]
 
     def __repr__(self):
         return '{}'.format(self.graph)
 
     def topological_sort(self):
         linear_ordering = []
-        in_degree = [
-            self.get_vertex_degree(vert)
-            for vert in range(1, self.number_of_vertices + 1)
-        ]
+        in_degree = self.vertex_in_degree[:]
         next_ = [
-            vertex + 1 for vertex in range(self.number_of_vertices)
+            vertex for vertex in range(self.number_of_vertices)
             if in_degree[vertex] == 0
         ]
 
         while next_:
+            print(next_)
+            print(in_degree)
             u = next_.pop()
             linear_ordering.append(u)
             for vert in self.graph[u]:
-                if not vert:
+                if vert is None:
                     break
-                in_degree[vert - 1] -= 1
-                if in_degree[vert - 1] == 0:
+                in_degree[vert] -= 1
+                if in_degree[vert] == 0:
                     next_.append(vert)
 
         return linear_ordering
