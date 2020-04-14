162d771f2c7a3e0f32894d03f87fd8101e7bceae
George Stambulyants <george.stamb28@gmail.com>
2020-04-14 17:48:41 +0300

    Rewrite shortest paths in orinted acyclic graphs. Not finished yet. <<Introduction to the algorithms>>

--- IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/shortest_paths_in_oriented_acyclic_graphs.py
+++ IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/shortest_paths_in_oriented_acyclic_graphs.py
@@ -1,33 +1,20 @@
 from .topological_sort import Graph
 
+
 infinity = float('inf')
 
 
 class Graph(Graph):
-    def __init__(self, number_of_vertices: int):
-        self.start = 0
+    def __init__(self, number_of_vertices):
         self.number_of_vertices = number_of_vertices
-        self.shortest = [0] + [infinity] * self.number_of_vertices
-        self.pred = [None] * (self.number_of_vertices + 1)
-        self.graph = {
-            vertex: [] for vertex in range(1, self.number_of_vertices + 1)
-        }
-        self.weights = {}
+        self.graph = [[] for _ in range(self.number_of_vertices)]
+        self.vertex_in_degree = [0] * self.number_of_vertices
+        self.weight = {}
 
-    def add_edge(self, from_: int, to: int, weight: int):
+    def add_edge(self, from_, to, weight):
         self.graph[from_].append(to)
-        self.weights.update({
-            (from_, to): weight,
-        })
+        self.vertex_in_degree[to] += 1
+        self.weight[(from_, to)] = weight
 
-    def get_weight(u: int, v: int) -> int:
-        return self.weights.get(u, v)
-
-    def relax(u: int, v: int):
-        '''
-            u, v - two vertices
-            (u, v) - edge
-        '''
-        if self.shortest[u] + self.get_weight(u, v) < self.shortest[v]:
-            self.shortest[v] = self.shortest[u] + self.get_weight(u, v)
-            self.pred[v] = u
+    def get_weight(self, u, v):
+        return self.weight.get(u, v)
