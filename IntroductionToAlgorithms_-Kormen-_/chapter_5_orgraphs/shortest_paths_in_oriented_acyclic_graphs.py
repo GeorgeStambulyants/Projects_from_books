46537ad30530ec9d0f0ff6ca3777514ca649ac93
George Stambulyants <george.stamb28@gmail.com>
2020-04-17 17:32:26 +0300

    Finished the algorithms. The idea is clear. But my implementetion wasn't tested yet. <<Introduction to the algorithms>>

--- IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/shortest_paths_in_oriented_acyclic_graphs.py
+++ IntroductionToAlgorithms_-Kormen-_/chapter_5_orgraphs/shortest_paths_in_oriented_acyclic_graphs.py
@@ -1,4 +1,6 @@
-from .topological_sort import Graph
+# Time: O(n + m), where n -- number of vertices and m -- number of edges
+
+from topological_sort import Graph
 
 
 infinity = float('inf')
@@ -10,6 +12,11 @@ class Graph(Graph):
         self.graph = [[] for _ in range(self.number_of_vertices)]
         self.vertex_in_degree = [0] * self.number_of_vertices
         self.weight = {}
+        self.shortest = [infinity] * (self.number_of_vertices + 1)
+        self.pred = [None] * (self.number_of_vertices + 1)
+        self.fiction_start = number_of_vertices
+        self.shortest[self.fiction_start] = 0
+        self.pred[self.fiction_start] = None
 
     def add_edge(self, from_, to, weight):
         self.graph[from_].append(to)
@@ -18,3 +25,20 @@ class Graph(Graph):
 
     def get_weight(self, u, v):
         return self.weight.get(u, v)
+
+    def relax(self, u, v):
+        if self.shortest[u] + self.get_weight(u, v) < self.shortest[v]:
+            self.shortest[v] = self.shortest[u] + self.get_weight(u, v)
+            self.pred[v] = u
+
+    def dag_shortest_paths(self):
+        linear_order = self.topological_sort()
+
+        for i in range(len(self.number_of_vertices)):
+            self.shortest[i] = infinity
+            self.pred[i] = None
+        self.shortest[self.number_of_vertices] = 0
+
+        for u in linear_order:
+            for v in self.graph[u]:
+                self.relax(u, v)
