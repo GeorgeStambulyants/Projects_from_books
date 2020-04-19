d57073d250c1f769b8330a70978065570fae5de8
George Stambulyants <george.stamb28@gmail.com>
2020-04-19 21:21:34 +0300

    url, placeholder view, and first-cut templates for my_lists. <<TDD with Python>>

--- TDD_with_Python/part_1/superlists/lists/urls.py
+++ TDD_with_Python/part_1/superlists/lists/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from lists import views
 
 
 urlpatterns = [
     path('new', views.new_list, name='new_list'),
     path('<int:id>/', views.view_list, name='view_list'),
+    re_path(r'users/(.+)/', views.my_lists, name='my_lists')
 ]
