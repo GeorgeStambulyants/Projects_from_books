b6aecbf464880c30d65e7b9001af9f15b1171bc5
George Stambulyants <george.stamb28@gmail.com>
2020-05-03 23:06:55 +0300

    Add input validation functions. <<JavaScript By Example>>

--- JavaScriptByExample/eventRegistrationApp/src/js/home.js
+++ JavaScriptByExample/eventRegistrationApp/src/js/home.js
@@ -1,4 +1,6 @@
 import './general';
+import validateRegistrationForm from 
+'./services/formValidation/validateRegistrationForm';
 
 class Home {
     constructor() {
@@ -20,6 +22,49 @@ class Home {
     onFormSubmit(event) {
         event.preventDefault();
         const formValues = this.getFormValues();
+        const formStatus = validateRegistrationForm(formValues);
+
+        if (formStatus.isValid) {
+            this.clearErrors();
+            this.submitForm(formValues);
+        } else {
+            this.clearErrors();
+            this.highlightErrors(formStatus.result);
+        }
+    }
+
+    clearErrors() {
+        this.$username.parentElement.classList.remove('has-error');
+        this.$phone.parentElement.classList.remove('has-error');
+        this.$email.parentElement.classList.remove('has-error');
+        this.$age.parentElement.classList.remove('has-error');
+        this.$profession.parentElement.classList.remove('has-error');
+        this.$experience.parentElement.classList.remove('has-error');
+    }
+
+    highlightErrors(result) {
+        if (!result.username) {
+            this.$username.parentElement.classList.add('has-error');
+        }
+        if (!result.phone) {
+            this.$phone.parentElement.classList.add('has-error');
+        }
+        if (!result.email) {
+            this.$email.parentElement.classList.add('has-error');
+        }
+        if (!result.age) {
+            this.$age.parentElement.classList.add('has-error');
+        }
+        if (!result.profession) {
+            this.$professionb.parentElement.classList.add('has-error');
+        }
+        if (!result.experience) {
+            this.$experience.parentElement.classList.add('has-error');
+        }
+    }
+
+    submitForm() {
+        
     }
 
     getFormValues() {
