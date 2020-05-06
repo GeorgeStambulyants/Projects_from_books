6098b2cb6554e3f6e8b14a35ed4fa309768bf3b2
George Stambulyants <george.stamb28@gmail.com>
2020-05-06 11:35:20 +0300

    Configute orders app to interact with payment app. <<MeleBook>>

--- MeleBook/Shop/myshop/orders/views.py
+++ MeleBook/Shop/myshop/orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from .models import OrderItem
 from .forms import OrderCreateForm
@@ -21,10 +21,8 @@ def order_create(request):
                 )
             cart.clear()
             order_created.delay(order.id)
-            return render(
-                request, 'orders/order/created.html',
-                {'order': order}
-            )
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     
