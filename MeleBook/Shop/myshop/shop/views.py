9210bc212fd69abac25c7d7193d58c304de5f7d0
George Stambulyants <george.stamb28@gmail.com>
2020-05-08 16:34:09 +0300

    Add recommendation engin with redis. <<MeleBook>>

--- MeleBook/Shop/myshop/shop/views.py
+++ MeleBook/Shop/myshop/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
-from . models import Category, Product
+from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .recommender import Recommender
 
 
 def product_list(request, category_slug=None):
@@ -37,10 +38,13 @@ def product_detail(request, id, slug):
         available=True,
     )
     cart_product_form = CartAddProductForm()
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
     return render(
         request, 'shop/product/detail.html',
         {
             'product': product,
             'cart_product_form': cart_product_form,
+            'recommended_products': recommended_products,
         }
     )
