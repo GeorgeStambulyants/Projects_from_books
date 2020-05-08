2834bc9fb8afffd858b68805282d1420fed5dfe4
George Stambulyants <george.stamb28@gmail.com>
2020-05-08 12:22:35 +0300

    BugFix. <<MeleBook>>

--- MeleBook/Shop/myshop/cart/forms.py
+++ MeleBook/Shop/myshop/cart/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from djangio.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
