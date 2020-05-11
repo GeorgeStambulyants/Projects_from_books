b9d4da1cecf0aae26d315bd7d043dcdfccd37828
George Stambulyants <george.stamb28@gmail.com>
2020-05-11 13:41:47 +0300

    Add views, templates and urlpatterns to display courses. <<MeleBook>>

--- MeleBook/E-Learning_Platform/educa/courses/views.py
+++ MeleBook/E-Learning_Platform/educa/courses/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import (
     CreateView, UpdateView, DeleteView
 )
@@ -10,11 +11,38 @@ from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
 
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 
-from .models import Course, Content, Module
+from .models import Course, Content, Module, Subject
 from .forms import ModuleFormSet
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses,
+        })
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 class ContentCreateUpdateView(TemplateResponseMixin, View):
