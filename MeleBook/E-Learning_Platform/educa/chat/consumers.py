385480b5ea2b2d571e95cf31a3a10ca389279386
Stambulyants George <george.stamb28@gmail.com>
2020-05-14 15:16:11 +0300

    Add context to the messages. <<MeleBook>>

--- MeleBook/E-Learning_Platform/educa/chat/consumers.py
+++ MeleBook/E-Learning_Platform/educa/chat/consumers.py
@@ -1,5 +1,7 @@
 import json
 
+from django.utils import timezone
+
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
@@ -7,6 +9,7 @@ from asgiref.sync import async_to_sync
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         async_to_sync(self.channel_layer.group_add)(
@@ -24,11 +27,15 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
+
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
