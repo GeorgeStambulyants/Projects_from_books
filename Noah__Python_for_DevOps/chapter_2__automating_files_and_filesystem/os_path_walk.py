a486c0485bf8566354de34ed0fea865c737cf2bf
George Stambulyants <george.stamb28@gmail.com>
2020-05-17 19:05:02 +0300

    Rewrite walk_path func using os.walk func. <<DevOps_with_Python__Noah>>

--- Noah__Python_for_DevOps/chapter_2__automating_files_and_filesystem/os_path_walk.py
+++ Noah__Python_for_DevOps/chapter_2__automating_files_and_filesystem/os_path_walk.py
@@ -7,19 +7,18 @@ if len(sys.argv) > 1:
 
 
 def walk_path(parent_path):
-    print(f'Checking: {parent_path}')
-    childs = os.listdir(parent_path)
+    for parent_path, directories, files in os.walk(parent_path):
+        print(f'Checking: {parent_path}')
 
-    for child in childs:
-        child_path = os.path.join(parent_path, child)
-        if os.path.isfile(child_path):
-            last_access = os.path.getatime(child_path)
-            size = os.path.getsize(child_path)
-            print(f'File: {child_path}')
+        for file_name in files:
+            file_path = os.path.join(parent_path, file_name)
+
+            last_access = os.path.getatime(file_path)
+            size = os.path.getsize(file_path)
+
+            print(f'File: {file_path}')
             print(f'\tlast accessed: {last_access}')
             print(f'\tsize: {size}')
-        elif os.path.isdir(child_path):
-            walk_path(child_path)
 
 
 if __name__ == '__main__':
