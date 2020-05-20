3a65f4126b66e6645a63233e9cf08969ab09a4f5
George Stambulyants <george.stamb28@gmail.com>
2020-05-20 14:34:11 +0300

    Add simple example with flask and molotov. <<DevOps with Python>>

---
+++ DevOps_with_Python__Noah/chapter_4__linux_utilities/small.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, request
+
+
+app = Flask('basic app')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        redirect('https://www.google.com/search?q={}'.format(
+            request.args['q'])
+        )
+    else:
+        return '<h1>GET request from Flask!</h1>'
