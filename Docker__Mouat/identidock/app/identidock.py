a08a7b56dcc705a32c5e7283c33d3d02aa3cd371
George Stambulyants <george.stamb28@gmail.com>
2020-05-24 13:52:49 +0300

    Add dnmonster to generate identiconds. Also update Dockerfile with adding requests to pip. <<Docker__Mouat>>

--- Docker__Mouat/identidock/app/identidock.py
+++ Docker__Mouat/identidock/app/identidock.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+import requests
 
 
 app = Flask(__name__)
@@ -7,10 +9,18 @@ default_name = 'Joe Bloggs'
 
 
 @app.route('/')
-def get_identicon():
+def mainpage():
     name = default_name
 
     return render_template('home.html', name=name)
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+    request = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = request.content
+
+    return Response(image, mimetype='image/png')
 
 
 if __name__ == "__main__":
