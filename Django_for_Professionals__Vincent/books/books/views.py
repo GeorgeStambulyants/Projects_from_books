e11e42b99b44b959b048c6f30a0746842c6bf8a1
George Stambulyants <george.stamb28@gmail.com>
2020-06-04 10:36:58 +0300

    Add custom permission

--- Django_for_Professionals__Vincent/books/books/views.py
+++ Django_for_Professionals__Vincent/books/books/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, PermissionRequiredMixin
+)
 
 from . models import Book
 
@@ -11,8 +13,9 @@ class BookListView(LoginRequiredMixin, ListView):
     login_url = 'account_login'
 
 
-class BookDetailView(LoginRequiredMixin, DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
+    permission_required = 'books.special_status'
