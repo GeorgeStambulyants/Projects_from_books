6aa59a1400e77bc75bd451420bd252fdfe1a872b
George Stambulyants <george.stamb28@gmail.com>
2020-06-05 08:54:20 +0300

    Add simple search feature

--- Django_for_Professionals__Vincent/books/books/views.py
+++ Django_for_Professionals__Vincent/books/books/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, PermissionRequiredMixin
 )
+from django.db.models import Q
 
 from . models import Book
 
@@ -25,3 +26,10 @@ class SearchResultsListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
