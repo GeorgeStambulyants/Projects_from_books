cc927a5f694bf020b9b81d7b25066971f325c3c8
George Stambulyants <george.stamb28@gmail.com>
2020-06-12 11:54:59 +0300

    Add API docs based on API schema

--- Django_for_APIs__Vincent/blogapi/blog_project/urls.py
+++ Django_for_APIs__Vincent/blogapi/blog_project/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
-schema_view = get_schema_view(title='Blog API')
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts'
+schema_view = get_schema_view(title=API_TITLE)
 
 
 urlpatterns = [
@@ -15,6 +18,10 @@ urlpatterns = [
     path(
         'api/v1/rest-auth/registration/',
         include('rest_auth.registration.urls')
+    ),
+    path(
+        'docs/',
+        include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)
     ),
     path('schema/', schema_view),
 ]
