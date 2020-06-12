709cb2116888f65969916197cb2fa9a343495b77
George Stambulyants <george.stamb28@gmail.com>
2020-06-12 11:18:34 +0300

    Add serializers, views and endpoints to list existing users and each one individually

--- Django_for_APIs__Vincent/blogapi/posts/views.py
+++ Django_for_APIs__Vincent/blogapi/posts/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics, permissions
 
 from . models import Post
-from . serializers import PostSerializer
+from . serializers import PostSerializer, UserSerializer
 from . permissions import IsAuthorOrReadOnly
 
 
@@ -14,3 +16,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly, permissions.IsAuthenticated)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
