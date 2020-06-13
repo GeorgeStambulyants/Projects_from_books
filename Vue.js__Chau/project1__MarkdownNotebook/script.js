fa6203f84556d538f353d1f439b2b49b8bc10a93
George Stambulyants <george.stamb28@gmail.com>
2020-06-13 13:17:16 +0300

    Add initial simple functionality and styles

--- Vue.js__Chau/project1__MarkdownNotebook/script.js
+++ Vue.js__Chau/project1__MarkdownNotebook/script.js
@@ -1,9 +1,15 @@
-new Vue({
+var app = new Vue({
     el: '#notebook',
 
     data() {
         return {
             content: 'This is note'
         }
-    }
+    },
+
+    computed: {
+        notePreview() {
+            return marked(this.content);
+        },
+    },
 })
