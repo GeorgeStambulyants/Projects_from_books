2803355508fa340441ad55347c88ff589d8fa7d7
George Stambulyants <george.stamb16@gmail.com>
2020-06-16 19:44:59 +0300

    pep8 fixes

--- Django__Aratyn/MyMDB/django/user/urls.py
+++ Django__Aratyn/MyMDB/django/user/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from user import views
 from django.contrib.auth import views as auth_views
+from django.urls import path
 
+from . import views
 
 app_name = 'user'
 
@@ -9,5 +9,4 @@ urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
-
 ]
