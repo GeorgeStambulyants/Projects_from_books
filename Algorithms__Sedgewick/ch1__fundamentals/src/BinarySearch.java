468404005b63d1420c2ac8cb96f1991b5c3a002c
George Stambulyants <george.stamb16@gmail.com>
2020-06-22 13:54:19 +0300

    Remove depricated static method and add docs

--- Algorithms__Sedgewick/ch1__fundamentals/src/BinarySearch.java
+++ Algorithms__Sedgewick/ch1__fundamentals/src/BinarySearch.java
@@ -1,12 +1,39 @@
+/*
+    class BinarySearch must be tested with test data.
+    It can be find at https://algs4.cs.princeton.edu/code/
+    -------------------------------------------------------
+    Test with following files:
+        * tinyW.txt and tinyT.txt
+        * largeW.txt and largeT.txt
+    Testing format after compilation:
+        1) $ java BinarySearch tinyW.txt < tinyT.txt
+            output:
+                50
+                99
+                13
+        2) $ java BinarySearch largeW.txt < largeT.txt
+            output is very long and omitted here
+ */
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
 
+/*
+  The class provides a static method for binary
+  searching for an integer in a sorted array of integers.
+
+  The indexOf operations takes logarithmic time in the worst case.
+ */
 
 public class BinarySearch {
-    public static int rank(int key, int[] a) {
+
+    // This class should not be instantiated
+    private BinarySearch() { }
+
+    public static int indexOf(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
 
@@ -24,14 +51,15 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] whitelist = In.readInts(args[0]);
+        In in = new In(args[0]);
+        int[] allowList = in.readAllInts();
 
-        Arrays.sort(whitelist);
+        Arrays.sort(allowList);
 
         while (!StdIn.isEmpty()) {
             // Read key, print if not in whitelist
             int key = StdIn.readInt();
-            if (rank(key, whitelist) < 0)
+            if (BinarySearch.indexOf(key, allowList) < 0)
                 StdOut.println(key);
         }
     }
