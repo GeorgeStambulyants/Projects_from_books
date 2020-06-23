3e458c2cef2d5a399416e7c9e53e44eeadddf929
George Stambulyants <george.stamb16@gmail.com>
2020-06-23 14:05:10 +0300

    Add Exercises class

---
+++ Algorithms__Sedgewick/ch1__fundamentals/src/Exercises.java
@@ -0,0 +1,27 @@
+/*
+* All exercise solutions of the 1.1 paragraph
+* TODO: rewrite this heading
+* */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercises {
+    public static void transposition(int[][] array, int M, int N) {
+        /* TODO
+            Prints transposition of a two-dimensional array
+            with M rows and N columns
+         */
+    }
+
+    public static int lg(int N) {
+        /* TODO
+            Returns the largest int not larger
+            than the base-2 logarithm of N. Can not use Math
+         */
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
