3c0aab6c5bcd0388b067acdca26df9dec910d2f7
George Stambulyants <george.stamb16@gmail.com>
2020-06-26 17:53:33 +0300

    Add more code examples

---
+++ Algorithms__Sedgewick/ch1__fundamentals/src/code_examples/Counter.java
@@ -0,0 +1,38 @@
+package code_examples;
+
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Counter {
+    private final String name;
+    private int count;
+
+    public Counter(String id) {
+        this.name = id;
+    }
+
+    public void increment() {
+        this.count++;
+    }
+
+    public int tally(){
+        return this.count;
+    }
+
+    public String toString() {
+        return this.count + " " + this.name;
+    }
+
+    public static void main(String[] args) {
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+
+        heads.increment();
+        heads.increment();
+        heads.increment();
+        tails.increment();
+
+        StdOut.println(heads + " " + tails);
+        StdOut.println(heads.tally() + tails.tally());
+    }
+}
