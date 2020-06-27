4a0f450991165cc4a4750d097c6eb24e0c52bd05
George Stambulyants <george.stamb16@gmail.com>
2020-06-27 12:26:49 +0300

    Add more Java code examples

---
+++ Algorithms__Sedgewick/ch1__fundamentals/src/code_examples/StaticSETofInts.java
@@ -0,0 +1,37 @@
+package code_examples;
+
+import java.util.Arrays;
+
+
+public class StaticSETofInts {
+    private int[] a;
+
+    public StaticSETofInts(int[] keys) {
+        this.a = new int[keys.length];
+
+        for (int i = 0; i < keys.length; i++)
+            this.a[i] = keys[i];
+        Arrays.sort(a);
+    }
+
+    public boolean contains(int key) {
+        return this.rank(key) != -1;
+    }
+
+    public int rank(int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
---
+++ Algorithms__Sedgewick/ch1__fundamentals/src/code_examples/Whitelist.java
@@ -0,0 +1,20 @@
+package code_examples;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Whitelist {
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] w = in.readAllInts();
+        StaticSETofInts set = new StaticSETofInts(w);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (set.rank(key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
