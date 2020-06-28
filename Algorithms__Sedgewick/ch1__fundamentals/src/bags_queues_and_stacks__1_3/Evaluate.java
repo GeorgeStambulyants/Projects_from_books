cfb814626c6d60b414946949698b2f3d52903361
George Stambulyants <george.stamb16@gmail.com>
2020-06-28 13:30:20 +0300

    Dijkstra's Two-Stack Algorithm for Expression Evaluation

---
+++ Algorithms__Sedgewick/ch1__fundamentals/src/bags_queues_and_stacks__1_3/Evaluate.java
@@ -0,0 +1,56 @@
+/*
+    Dijkstra's Two-Stack Algorithm for Expression Evaluation
+
+    This code assumes that the expression is
+    fully parenthesized, with numbers and characters separated
+    by whitespaces
+ */
+package bags_queues_and_stacks__1_3;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Evaluate {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<String>();
+        Stack<Double> vals = new Stack<Double>();
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+
+            if (s.equals("("))
+                ;
+            else if (s.equals("+"))
+                ops.push(s);
+            else if (s.equals("-"))
+                ops.push(s);
+            else if (s.equals("*"))
+                ops.push(s);
+            else if (s.equals("/"))
+                ops.push(s);
+            else if (s.equals("sqrt"))
+                ops.push(s);
+            else if (s.equals(")")) {
+                String op = ops.pop();
+                double v = vals.pop();
+
+                if (op.equals("+"))
+                    v = vals.pop() + v;
+                else if (op.equals("-"))
+                    v = vals.pop() - v;
+                else if (op.equals("*"))
+                    v = vals.pop() * v;
+                else if (op.equals("/"))
+                    v = vals.pop() / v;
+                else if (op.equals("sqrt"))
+                    v = Math.sqrt(v);
+                vals.push(v);
+            }
+            else
+                vals.push(Double.parseDouble(s));
+        }
+        StdOut.println(vals.pop());
+    }
+}
