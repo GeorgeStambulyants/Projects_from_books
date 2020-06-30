cb129bb0acac12e53e4a980e3b0c4b4b25c20c92
George Stambulyants <george.stamb16@gmail.com>
2020-06-30 22:04:45 +0300

    Code refactoring

--- Django__Mele/Blog/mysite/blog/templatetags/blog_tags.py
+++ Django__Mele/Blog/mysite/blog/templatetags/blog_tags.py
@@ -1,11 +1,14 @@
 from django import template
 from django.db.models import Count
 from django.utils.safestring import mark_safe
-from ..models import Post
+
 import markdown
+
+from ..models import Post
 
 
 register = template.Library()
+
 
 @register.simple_tag
 def total_posts():
@@ -20,8 +23,9 @@ def show_latest_posts(count=5):
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
-    return Post.published.annotate(total_comments=Count('comments'))\
-        .order_by('-total_comments')[:count]
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
 
 
 @register.filter(name='markdown')
