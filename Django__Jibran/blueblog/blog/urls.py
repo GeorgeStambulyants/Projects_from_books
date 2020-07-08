c8fe0c8411b79151929786d5485f3cdb54c94224
George Stambulyants <george.stamb16@gmail.com>
2020-07-08 18:16:16 +0300

    Let users create blogs

--- Django__Jibran/blueblog/blog/urls.py
+++ Django__Jibran/blueblog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import NewBlogView
+
 
 urlpatterns = [
-
+    path('new/', NewBlogView.as_view(), name='new_blog'),
 ]
--- Django__Jibran/blueblog/config/urls.py
+++ Django__Jibran/blueblog/config/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
 ]
