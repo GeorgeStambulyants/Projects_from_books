9fa94c944be384850a8f3d5587648fc6e80820b1
George Stambulyants <george.stamb16@gmail.com>
2020-07-15 16:33:54 +0300

    Add home page

--- Django__Jibran/blueblog/config/urls.py
+++ Django__Jibran/blueblog/config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+
+from blog.views import HomeView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', HomeView.as_view(), name='home'),
 ]
