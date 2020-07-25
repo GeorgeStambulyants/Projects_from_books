bb91bc60673ed7899547ff7ef7a46d0dfc0b2446
George Stambulyants <george.stamb16@gmail.com>
2020-07-25 19:04:37 +0300

    Remove Django__Jibran

--- Django__Jibran/blueblog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import NewBlogView, UpdateBlogView
-
-
-urlpatterns = [
-    path('new/', NewBlogView.as_view(), name='new_blog'),
-    path('<int:pk>/update/', UpdateBlogView.as_view(), name='update_blog')
-]
