fef988cd76f856c66b4fcbaf4cf1283106b32250
George Stambulyants <george.stamb16@gmail.com>
2020-08-21 17:23:13 +0300

    Use Post/Redirect/Get pattern in index view

--- Flask__Grinberg/flasky/hello.py
+++ Flask__Grinberg/flasky/hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -24,13 +24,12 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=name)
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
