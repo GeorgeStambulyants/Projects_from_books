7bed58f88299a13739532f8ddbeade95b8b3ede9
George Stambulyants <george.stamb16@gmail.com>
2020-11-05 11:08:31 +0300

    add hiLo project

---
+++ Java__Payne/hiLo/src/hiLo/HiLo.java
@@ -0,0 +1,12 @@
+package hiLo;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		// generate random number
+		int theNumber = (int)(Math.random() * 100 + 1);
+		System.out.println(theNumber);
+
+	}
+
+}
