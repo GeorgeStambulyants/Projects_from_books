c5c096cf097d40f164a189cc11fc004ef84d9783
George Stambulyants <george.stamb16@gmail.com>
2020-11-25 13:14:02 +0300

    Add blog and organizer apps

--- Django_Unleashed__Pinkham/suorganizer_project/config/settings.py
+++ Django_Unleashed__Pinkham/suorganizer_project/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'organizer.apps.OrganizerConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
